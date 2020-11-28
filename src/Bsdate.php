369f2eefb22d585c49af736e3322ab904287a859
Padam Shankhadev <shankhadev123@gmail.com>
2020-11-28 12:53:44 +0545

    php 8 support

--- src/Bsdate.php
+++ src/Bsdate.php
@@ -507,7 +507,7 @@ final class Bsdate implements BsdateInterface
         $convertedNumber = str_replace(
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
             ['०', '१', '२', '३', '४', '५', '६', '७', '८', '९'],
-            $str
+            (string)$str
         );
 
         return $convertedNumber;
